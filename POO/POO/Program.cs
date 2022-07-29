0800e93670b641eb121c4ed9ad9889123e829800
Wagner Costa <wagnercosta2200@gmail.com>
2022-07-28 21:21:44 -0300

    POO Abstração

---
+++ POO/POO/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "Wagner";
+            p1.Idade = 46;
+
+            p1.Apresentar();
+        }
+    }
+}
