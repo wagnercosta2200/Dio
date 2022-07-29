7ccfbe959c76f708bf7bef09de5d22020487114c
Wagner Costa <wagnercosta2200@gmail.com>
2022-07-28 21:55:20 -0300

    Encapsulamento

---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,26 @@
+﻿// Encapsulamento   serve para proteger uma classe e definir limites para alteração de suas propriedaes
+
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //valores validos
+            Retangulo r = new Retangulo();
+            r.DefenirMedidadas(30, 30);
+
+            Console.WriteLine($"Area : { r.ObterArea()}");
+
+            //valores invalidos
+            Retangulo r2 = new Retangulo();
+            r2.DefenirMedidadas(0, 30);
+
+            Console.WriteLine($"Area : { r2.ObterArea()}");
+
+        }
+    }
+}
+
+
---
+++ Encapsulamento/Encapsulamento/Retangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class Retangulo
+    {
+        private double comprimento;
+        private double largura;
+        private bool valido;
+
+        public void DefenirMedidadas(double comprimento, double largura)
+        {
+            if(comprimento > 0 && largura > 0)
+            {
+                this.comprimento = comprimento;
+                this.largura = largura;
+                valido = true;  
+
+            }
+            else
+            {
+                Console.WriteLine("Valores invalidos");
+            }
+          
+        }
+
+        public double ObterArea()
+        {
+            if (valido)
+            {
+                return comprimento * largura;
+            }
+            else
+            {
+                Console.WriteLine("Preencha valores validos");
+                return 0;
+            }
+        }
+    }
+}
