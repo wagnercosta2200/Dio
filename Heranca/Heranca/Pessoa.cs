4bb36ea248d0a4581116f2e2ae641cafc87fa029
Wagner Costa <wagnercosta2200@gmail.com>
2022-08-03 20:47:52 -0300

    Herança

---
+++ Heranca/Heranca/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Ola, meu nome é{Nome}e tenho {Idade} anos");
+        }
+    }
+}
---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno p1 = new Aluno();
+            p1.Nome = "Wagner";
+            p1.Idade = 46;
+
+            p1.Apresentar();
+        }
+    }
+}
+
