0eb8c61d950d074cfe4fdb966dd254eff7dbe31c
Wagner Costa <wagnercosta2200@gmail.com>
2022-08-04 18:37:18 -0300

    polimorfismo

--- Polimorfismo/Polimorfismo/Program.cs
+++ Polimorfismo/Polimorfismo/Program.cs
@@ -6,11 +6,17 @@ namespace Polimorfismo// sobrescreve metodos das classes filhas para se comporta
     {
         static void Main(string[] args)
         {
-            Aluno p1 = new Aluno();
-            p1.Nome = "Wagner";
-            p1.Idade = 46;
-            p1.Nota = 10;
-            p1.Apresentar();
+            Calculadora calc = new Calculadora();
+            Console.WriteLine("Resultado da pimeira soma: " + calc.Somar(10,10));
+            Console.WriteLine("Resultado da segunda soma: " + calc.Somar(10, 10, 9));
+
+
+
+            //Aluno p1 = new Aluno();
+            //p1.Nome = "Wagner";
+            //p1.Idade = 46;
+            //p1.Nota = 10;
+            //p1.Apresentar();
         }
 
     }
